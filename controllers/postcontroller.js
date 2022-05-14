65cf348b274c67245a9ece1e81c515015e277a96
raylu511 <raylu511@gmail.com>
2022-05-14 16:16:17 -0400

    Commented errors

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,59 +1,60 @@
-const postmodels = require('../models/postsmodels')
+const postmodels = require("../models/postsmodels");
 
+// Make sure to return after sending res back
 
 //HOME SCREEN
-const home = async(req, res) => {
-    res.send('This is our screen working')
-}
-
+const home = async (req, res) => {
+  res.send("This is our screen working");
+};
 
 //GET ALL POSTS
-const getAllPosts = async(req, res) => {
-    let selectAllPosts = await postmodels.getAllPostsDB()
-    res.status(200).json(selectAllPosts)
-}
+const getAllPosts = async (req, res) => {
+  const selectAllPosts = await postmodels.getAllPostsDB();
+  return res.status(200).json(selectAllPosts);
+};
 
-
+// Get a single post by postId (Not user id)
 //GET ALL POST FROM A SINGLE USER
-const getAllPostsSingleUser = async(req, res) => {
-    const userID = parseInt(req.params.id)
-    let selectSinglePost = await postmodels.getAllPostsSingleUserDB(userID)
-    res.status(200).json(selectSinglePost)
-}
+const getAllPostsSingleUser = async (req, res) => {
+  const userID = parseInt(req.params.id);
+  let selectSinglePost = await postmodels.getAllPostsSingleUserDB(userID);
+  res.status(200).json(selectSinglePost);
+};
 
+// /posts and get userId from req.body
 
 //MAKE NEW POST
-const createNewPost = async(req, res) => {
-    const userID = parseInt(req.params.id)
-    const {content} = req.body
-    let makeNewPost = await postmodels.createNewPostDB(userID, content)
-    res.status(200).json(makeNewPost)
-}
+const createNewPost = async (req, res) => {
+  const userID = parseInt(req.params.id);
+  const { content } = req.body;
+  let makeNewPost = await postmodels.createNewPostDB(userID, content);
+  res.status(200).json(makeNewPost);
+};
 
-
+// /post/:id no need for userId
 //UPDATE EXISTING BLOG
-const updatePost = async(req, res) => {
-    const userID = parseInt(req.params.id)
-    const postID = parseInt(req.params.postid)
-    const {content} = req.body
-    let fixPost = await postmodels.updatePostDB(content, userID, postID)
-    res.status(200).json(fixPost)
-}
+const updatePost = async (req, res) => {
+  const userID = parseInt(req.params.id);
+  const postID = parseInt(req.params.postid);
+  const { content } = req.body;
+  let fixPost = await postmodels.updatePostDB(content, userID, postID);
+  res.status(200).json(fixPost);
+};
 
-
+// /post/:id no need for userId
 //DELETE EXISTING POST
-const deletePost = async(req, res) => {
-    const userID = parseInt(req.params.id)
-    const postID = parseInt(req.params.postid)
-    let destroyPost = await postmodels.deleteOnePostDB(userID, postID)
-    res.status(200).json(destroyPost);
-}
+const deletePost = async (req, res) => {
+  const userID = parseInt(req.params.id);
+  const postID = parseInt(req.params.postid);
+  let destroyPost = await postmodels.deleteOnePostDB(userID, postID);
+  res.status(200).json(destroyPost);
+};
 
 module.exports = {
-    home,
-    getAllPosts,
-    getAllPostsSingleUser,
-    createNewPost,
-    updatePost,
-    deletePost
-}
+  home,
+  getAllPosts,
+  getAllPostsSingleUser,
+  createNewPost,
+  updatePost,
+  deletePost,
+};
--- routes/postroutes.js
+++ routes/postroutes.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const postcontrols = require('../controllers/postcontrollers')
-const Router = express.Router()
+const express = require("express");
+const postcontrols = require("../controllers/postcontrollers");
+const Router = express.Router();
 
 //HOME SCREEN
-Router.get('/home', postcontrols.home)
-
+Router.get("/home", postcontrols.home);
 
 //GET ALL BLOG
-Router.get('/posts', postcontrols.getAllPosts)
+Router.get("/posts", postcontrols.getAllPosts);
 
+// GET ALL BLOGS FROM A SINGLE USER
+// router.get(/users/:id/posts)
 
-//GET ALL BLOGS FROM A SINGLE USER
-Router.get('/posts/:id', postcontrols.getAllPostsSingleUser)
+// Get a single post by id
+Router.get("/posts/:id", postcontrols.getAllPostsSingleUser);
 
+// MAKE NEW BLOG
+// /posts
+// body:userId
 
-//MAKE NEW BLOG
-Router.post('/posts/:id', postcontrols.createNewPost)
+Router.post("/posts/:id", postcontrols.createNewPost);
 
-
+// /post/:id
+// id ^ here is postId
 //UPDATE EXISTING BLOG
-Router.put('/posts/:id/:postid', postcontrols.updatePost)
+Router.put("/posts/:id/:postid", postcontrols.updatePost);
 
-
+// /post/:id
 //DELETE EXISTING BLOG
-Router.delete('/posts/:id/:postid', postcontrols.deletePost)
+Router.delete("/posts/:id/:postid", postcontrols.deletePost);
+
+// Middleware that checks if the user is the same
 
 module.exports = Router;
