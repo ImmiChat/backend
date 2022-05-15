89cf15f0e7b8f5bf3e047974cd14e1f8199d32b3
raylu511 <raylu511@gmail.com>
2022-05-15 11:34:06 -0400

    Finished feed endpoint

--- seeds/03_comments_seed.js
+++ seeds/03_comments_seed.js
@@ -4,7 +4,7 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("comments").del();
+  
   await knex("comments").insert([
     { post_id: 1, user_id: 1, comment_text: "nice blog" },
   ]);
