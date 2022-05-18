1a8465730c244382fcc719dc61e99d05fbb2283b
KVCobbs <kenyacobbs9001@gmail.com>
2022-05-17 21:16:29 -0400

    for now

---
+++ routes/commentroutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const commentcontrols = require("../controllers/commentcontroller");
+const Router = express.Router();
+
+/*
+Router.get("/", commentsController.getCommentsOfBlog);
+
+//Router.get("/:id", likesController.getSingleComment);
+
+Router.post("/", commentsController.createCommentOfBlog);
+
+Router.put("/:id", commentsController.editCommentOfBlog);
+
+Router.delete("/:id", commentsController.deleteCommentOfBlog);
+
+module.exports = Router;
+*/
