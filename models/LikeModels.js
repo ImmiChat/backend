192d4ffc51a0b8e57959b2e333e31cb3a4b49e2d
Kenya Cobbs <43050927+KVCobbs@users.noreply.github.com>
2022-05-19 21:21:45 -0400

    wrote comments
    just wrote on how you can change things for the better

--- models/LikeModels.js
+++ models/LikeModels.js
@@ -2,11 +2,16 @@ const db = require('../db/db')
 const dbpool = require('../dbconfig')
 
 //GET ALL LIKES FROM A POSt
+
+//make sure that you use the proper text written in the migrations when you use your parameters. 
+//Also remember shorter function names and camel case <:)
 function GetLikesFromPost(post_id) {
+//This query right here is a good attempt but the likes and posts should be lower case 
     return dbpool.query(`SELECT LIKES FROM POSTS WHERE id = ${post_id}`)
     .then((results) => results.rows)
 }
 
+//these last two are a better example of what to do 
 function createALike(user_id, post_id) {
     return db("likes").insert({user_id, post_id}).returning("*")
 }
