213182a1b2b91eee0f649c6683315e69251de65f
Kenya Cobbs <43050927+KVCobbs@users.noreply.github.com>
2022-05-19 21:23:04 -0400

    Update likesroutes.js

--- routes/likesroutes.js
+++ routes/likesroutes.js
@@ -1,9 +1,10 @@
+//you dont need two routers. 
 const { Router } = require("express");
 const express = require ("express")
 const likeControls = require("../controllers/likeControllers")
 const Router = express.Router();
 
-
+//everything else here is fine thanks to ray reviewing but I hope you understand to change every function after the likeControls.
 // Get Likes From A Single Post
 Router.get("/post/:id/likes", likeControls.GetAllLikesFromAllPosts);
 
