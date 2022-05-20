05d2ecc77cc316c5f165c6242d688e94df0ad391
Kenya Cobbs <43050927+KVCobbs@users.noreply.github.com>
2022-05-19 21:19:13 -0400

    Added some comments.
    Will test after these comments are taken

--- controllers/likeControllers.js
+++ controllers/likeControllers.js
@@ -1,5 +1,8 @@
 const likeModels = require("../models/LikeModels")
 
+
+//Hey Damon. This isn't how to write functions. it's pretty bad camel case. Also it would be helpful to shorten the functions just for writing purposes. 
+// Also try to use try/catch error message in order to test this code out in your console before sending it to me to tetst. In order to make the process efficent make sure to work on changes and test the actual code
 const GetAllLikesFromASinglePost = async (req,res) => {
     const postID = req.params.id
     const GetLikesFromSinglePost = await likeModels.GetLikesFromPost(postID)
