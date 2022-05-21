c5ad3b4f883fad40a288998805fd900b88dc1ed9
raylu511 <raylu511@gmail.com>
2022-05-21 11:54:28 -0400

    Added get all users endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,11 @@
 const UserModel = require("../models/UserModel");
 
 class UserController {
+  static getAllUsers = async (req, res) => {
+    const users = await UserModel.getAllUsersFromDB();
+    return res.status(201).json(users);
+  };
+
   static updateUser = async (req, res) => {
     const userId = req.params.id;
     const updatedProfile = await UserModel.updateUserFromDB(req.body, userId);
